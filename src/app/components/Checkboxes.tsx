368daff681de89f1df4ded5c6611c395319634a6
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-04-27 04:42:38 +0900

    feat: ローディング中は都道府県の文字も非表示にした

--- src/app/components/Checkboxes.tsx
+++ src/app/components/Checkboxes.tsx
@@ -27,23 +27,26 @@ export const Checkboxes = ({
   }
   return (
     <div className={styles.mainDiv}>
-      {prefectures.map((prefecture, key) => (
-        <form className={styles.form} key={key}>
-          <input
-            className={styles.checkbox}
-            type='checkbox'
-            value={prefecture.prefCode}
-            checked={checkStatus[key].checked}
-            onChange={() => {
-              const newCheckStatus = [...checkStatus];
-              newCheckStatus[key].checked = !newCheckStatus[key].checked;
-              setCheckStatus(newCheckStatus);
-              updateCheckState(prefecture.prefCode, newCheckStatus);
-            }}
-          />
-          <label>{prefecture.prefName}</label>
-        </form>
-      ))}
+      <h2>都道府県</h2>
+      <div className={styles.checkboxArea}>
+        {prefectures.map((prefecture, key) => (
+          <form className={styles.form} key={key}>
+            <input
+              className={styles.checkbox}
+              type='checkbox'
+              value={prefecture.prefCode}
+              checked={checkStatus[key].checked}
+              onChange={() => {
+                const newCheckStatus = [...checkStatus];
+                newCheckStatus[key].checked = !newCheckStatus[key].checked;
+                setCheckStatus(newCheckStatus);
+                updateCheckState(prefecture.prefCode, newCheckStatus);
+              }}
+            />
+            <label>{prefecture.prefName}</label>
+          </form>
+        ))}
+      </div>
     </div>
   );
 };
